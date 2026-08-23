dce793d759d851c6bcc3aab1cc3bf052319d0153
Mikk <maidemikkegert@gmail.com>
2026-08-23 22:30:49 +0300

    Add clickjacking frame-busting guard

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { ProProvider } from './pro/ProProvider.tsx'
 
+if (window.top && window.top !== window.self) {
+  document.documentElement.style.display = 'none'
+  try {
+    window.top.location.replace(window.self.location.href)
+  } catch {
+    window.stop()
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ProProvider>
